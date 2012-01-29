35d02b7d7da95e70e8d33d1c7673348ef052b809
Jurriaan Bremer <jurriaanbremer@gmail.com>
2012-01-29 23:05:08 +0100

    initial sseify script

---
+++ sseify.py
@@ -0,0 +1,17 @@
+import sys, binascii, distorm3, enc
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		print 'Usage: python %s <machine-code>' % sys.argv[0]
+		exit(0)
+	
+	# get hex
+	code = binascii.unhexlify(sys.argv[1])
+	
+	# encode the instructions to sse
+	lines = []
+	for dis in distorm3.DecomposeGenerator(0, code, distorm3.Decode32Bits):
+		lines += enc.Enc(dis).encode()
+	
+	# print the instructions
+	print '\r\n'.join(lines)
