a02ac782176acfc0c2c978625ce0b9f050b382fb
Jurriaan Bremer <jurriaanbremer@gmail.com>
2012-02-13 16:38:54 +0100

    #3 improved and added test cases

--- tests.py
+++ tests.py
@@ -1,66 +1,59 @@
-import sys, enc, os, struct, subprocess
+import sys, enc, os, struct, subprocess, assemble
 from distorm3 import Registers, DecomposeGenerator, Decode32Bits
 
 ut = []
 count = 0
 
-def ut_test_write(lines):
-	global count ; count += 1
-	f = file('testing/%d.cpp' % count, 'w')
-	f.write('#include <stdio.h>\n#include <windows.h>\nDWORD WINAPI XmmThread(LPVOID){')
-	f.write('static unsigned long a[4] = {0x11111111, 0x22222222, 0x33333333, 0x44444444}, b[4] = {0x55555555, 0x66666666, 0x77777777, 0x88888888};')
-	f.write('__asm__(".intel_syntax noprefix \\n"\n"mov eax, %0 \\n"\n"movapd xmm6, [eax] \\n"\n"mov eax, %1 \\n"\n"movapd xmm7, [eax] \\n"')
-	f.write('"' + '\\n"\n"'.join(lines) + '\\n"\n')
-	f.write('".att_syntax"	:: "r" (a), "r" (b)	: "eax"); while (1); return 0; }')
-	f.write('int main(){HANDLE hThread = CreateThread(NULL, 0, XmmThread, NULL, 0, NULL); Sleep(10);')
-	f.write('CONTEXT Ctx = {}; Ctx.ContextFlags = CONTEXT_FULL | CONTEXT_EXTENDED_REGISTERS;')
-	f.write('GetThreadContext(hThread, &Ctx); fwrite(Ctx.ExtendedRegisters + 0xa0, 8, 16, stdout);}\n')
-	f.close()
-
 # unit test for register manipulation
 def ut_reg(hex, eax=0x11111111, ecx=0x22222222, edx=0x33333333, ebx=0x44444444, esp=0x55555555, ebp=0x66666666, esi=0x77777777, edi=0x88888888):
 	ut.append({'type': 'reg', 'hex': hex, 'regs': (eax % 2**32, ecx % 2**32, edx % 2**32, ebx % 2**32, esp % 2**32, ebp % 2**32, esi % 2**32, edi % 2**32)})
 
-def test(tests, compile):
+def test():
+	global count
 	for test in ut:
-		test['lines'] = [] ; test['asm'] = [] ; enc.Enc(None).reset_labels()
+		test['asm'] = [] ; enc.Enc(None).reset_labels()
+		test['lines'] = [
+			'jmp m128_init0_end', 'align 16, db 0', 'm128_init0: dd 0x11111111, 0x22222222, 0x33333333, 0x44444444', 'm128_init0_end:',
+			'jmp m128_init1_end', 'align 16, db 0', 'm128_init1: dd 0x55555555, 0x66666666, 0x77777777, 0x88888888', 'm128_init1_end:',
+			'movapd xmm6, dqword [m128_init0]', 'movapd xmm7, dqword [m128_init1]']
 		for dis in DecomposeGenerator(0, test['hex'].decode('hex'), Decode32Bits):
 			test['lines'] += enc.Enc(dis).encode()
 			test['asm'].append(str(dis))
-		ut_test_write(test['lines'])
-	
-	# only write the file..
-	if compile: return
-	
-	# 8 simultaneous jobs
-	os.system('make -j 8 ' + ' '.join(tests) + ' test')
-	
-	count = 0
-	for test in ut:
-		count += 1
+		test['code'] = assemble.assemble(test['lines'], 'testing/' + str(count))
+		
 		sys.stderr.write('Processing %s -> %-32s\r' % (test['hex'], ' ; '.join(test['asm'])))
-		output = struct.unpack('LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL', subprocess.Popen(['./testing/%d.exe' % count], stdout=subprocess.PIPE).stdout.read())
+		output = struct.unpack('LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL', assemble.Debuggable(test['code']).run())
 		if test['regs'] != output[24:]:
-			print '%s -> %s gave problems!' % (test['hex'], ' ; '.join(test['asm']))
+			print '%s -> %s gave problems (%d)!' % (test['hex'], ' ; '.join(test['asm']), count)
 			print 'Generated SSE Assembly:\n' + '\n'.join(test['lines']) + '\n'
 			# print all xmm registers
-			for i in xrange(8): print 'xmm%d 0x%08x 0x%08x 0x%08x 0x%08x' % (i, output[i*4+0], output[i*4+1], output[i*4+2], output[i*4+3])
+			for i in xrange(8): print 'xmm%d 0x%08x 0x%08x 0x%08x 0x%08x' % tuple([i] + list(output[i*4:i*4+4]))
 			# print all gpr's stored in xmm registers, 16 is offset of EAX in the Registers array
 			for i in xrange(8): print '%s 0x%08x -> 0x%08x' % (Registers[16+i], output[24+i], test['regs'][i])
 			print '' # newline
+			
+			assemble.Debuggable(test['code']).debug()
+		
+		count += 1
 
 def main():
-	compile = False
-	if len(sys.argv) == 1:
-		tests = ['mov', 'xor', 'add']
-	elif sys.argv[1] == 'compile':
-		tests = sys.argv[2:]
-		compile = True
-	else:
-		tests = sys.argv[1:]
+	index = None
+	tests = ['mov', 'xor', 'add']
+	if len(sys.argv) > 1:
+		argc = 1
+		try:
+			index = int(sys.argv[argc])
+			argc += 1
+		except:
+			index = None
+		
+		if argc != len(sys.argv):
+			tests = sys.argv[argc:]
 	
 	if not os.access('testing', os.R_OK):
 		os.mkdir('testing')
+	
+	print str(tests), index
 	
 	eax = 0x11111111
 	ax  = 0x1111
@@ -97,6 +90,9 @@ def main():
 		# xor edx, 0x13371337
 		ut_reg('81f237133713', edx=edx^0x13371337)
 		
+		# xor ebx, 0xdeadcafe
+		ut_reg('81f3fecaadde', ebx=ebx^0xdeadcafe)
+		
 		# xor al, al
 		ut_reg('32c0', eax=0x11111100)
 		
@@ -114,6 +110,12 @@ def main():
 		
 		# xor eax, ebx
 		ut_reg('33c3', eax=eax^ebx)
+		
+		# xor edx, eax
+		ut_reg('33d0', edx=edx^eax)
+		
+		# xor ecx, esi
+		ut_reg('33ce', ecx=ecx^esi)
 	
 	if 'mov' in tests:
 		# mov ebx, edx
@@ -141,7 +143,10 @@ def main():
 		# add ebx, 0xcafebabe
 		ut_reg('81c3bebafeca', ebx=ebx+0xcafebabe)
 	
-	test(tests, compile)
+	# only do a certain test
+	if index is not None: global ut ; ut = [ut[index]]
+	
+	test()
 
 if __name__ == '__main__':
 	main()
